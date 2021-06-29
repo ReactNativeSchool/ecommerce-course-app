7a73cecedcb57d94b4c4848cd5274db4b0a0a764
Spencer Carli <spencercarli@gmail.com>
2021-06-29 10:21:44 -0500

    Bottom Tab Navigator Setup

--- EcommerceApp/src/navigation/Main.js
+++ EcommerceApp/src/navigation/Main.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { List } from '../screens/List';
-import { TextDemo, ButtonDemo, FormDemo } from '../screens/Demos';
+const Temp = () => null;
+
+const MainTabs = createBottomTabNavigator();
+const Tabs = () => (
+  <MainTabs.Navigator>
+    <MainTabs.Screen name="Home" component={Temp} />
+    <MainTabs.Screen name="Explore" component={Temp} />
+    <MainTabs.Screen name="Account" component={Temp} />
+  </MainTabs.Navigator>
+);
 
 const MainStack = createStackNavigator();
 
 export const Main = () => (
   <MainStack.Navigator>
-    <MainStack.Screen name="List" component={List} />
-    <MainStack.Screen
-      name="TextDemo"
-      component={TextDemo}
-      options={{ headerTitle: 'Text Demo' }}
-    />
-    <MainStack.Screen
-      name="FormDemo"
-      component={FormDemo}
-      options={{ headerTitle: 'Button Demo' }}
-    />
-    <MainStack.Screen
-      name="ButtonDemo"
-      component={ButtonDemo}
-      options={{ headerTitle: 'Button Demo' }}
-    />
+    <MainStack.Screen name="Root" component={Tabs} />
   </MainStack.Navigator>
 );
