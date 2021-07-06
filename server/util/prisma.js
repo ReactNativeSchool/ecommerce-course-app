2fe3b418b1a7cc7f80b91cfdb59cee682d5df684
Spencer Carli <spencercarli@gmail.com>
2021-07-06 11:18:26 -0500

    Best Practice: Working with Prisma in a Serverless Environment

---
+++ server/util/prisma.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+// eslint-disable-next-line
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  prisma = global.prisma;
+}
+
+export default prisma;
