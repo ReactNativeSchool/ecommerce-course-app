4997fa89458b2fcdc6b96f7640df2719f6a93146
Spencer Carli <spencercarli@gmail.com>
2021-07-06 11:09:05 -0500

    Home Screen: Create endpoint for data

---
+++ server/pages/api/products/trending.js
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const selectFields = {
+  id: true,
+  name: true,
+  image: true,
+  price: true,
+};
+
+export default async (req, res) => {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed.' });
+  }
+
+  const bestSellers = await prisma.product.findMany({
+    where: {
+      soldCount: {
+        gt: 0,
+      },
+    },
+    orderBy: {
+      soldCount: 'desc',
+    },
+    take: 3,
+    select: selectFields,
+  });
+
+  const newItems = await prisma.product.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    take: 3,
+    select: selectFields,
+  });
+
+  const data = [
+    {
+      title: 'Best Sellers',
+      items: bestSellers,
+    },
+    {
+      title: 'New Items',
+      items: newItems,
+    },
+  ];
+
+  return res.status(200).json({ data });
+};
