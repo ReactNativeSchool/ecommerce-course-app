3ed9ee99227e066842baf645cbab37dc3e4e3c50
Spencer Carli <spencercarli@gmail.com>
2021-07-06 15:27:29 -0500

    Home Screen: Get data on mobile

---
+++ EcommerceApp/src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+import colors from '../constants/colors';
+
+export const Loading = () => (
+  <View style={{ paddingVertical: 20 }}>
+    <ActivityIndicator size="large" color={colors.brand} />
+  </View>
+);
--- EcommerceApp/src/screens/Home.js
+++ EcommerceApp/src/screens/Home.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { Button } from '../components/Button';
+import { useHomeData } from '../util/api';
+import { Loading } from '../components/Loading';
 
 export const Home = ({ navigation }) => {
+  const response = useHomeData();
+
+  if (response.isLoading) {
+    return <Loading />;
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
       <Button onPress={() => navigation.push('Details')}>Details</Button>
