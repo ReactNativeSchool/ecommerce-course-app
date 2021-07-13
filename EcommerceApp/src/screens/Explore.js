754327bffde51b16d8f5e3d024ac65379065b549
Spencer Carli <spencercarli@gmail.com>
2021-07-13 10:21:01 -0500

    Explore Page: Render data

--- EcommerceApp/src/screens/Explore.js
+++ EcommerceApp/src/screens/Explore.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useExploreData } from '../util/api';
 import { Loading } from '../components/Loading';
+import { ProductList } from '../components/List';
 
 export const Explore = () => {
   const { isLoading, data } = useExploreData();
@@ -10,6 +11,14 @@ export const Explore = () => {
     return <Loading />;
   }
 
-  console.log(data);
-  return null;
+  const sections = data?.categories?.map(category => {
+    return {
+      ...category,
+      title: category.name,
+      data: category.products,
+      products: undefined,
+    };
+  });
+
+  return <ProductList sections={sections} />;
 };
