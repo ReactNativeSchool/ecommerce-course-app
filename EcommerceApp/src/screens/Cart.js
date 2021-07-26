24e75f3d0eff5b0fbd063bc76a1118bd7f278694
Spencer Carli <spencercarli@gmail.com>
2021-07-26 15:19:37 -0500

    Cart Management: Calculate Cart Total

--- EcommerceApp/src/screens/Cart.js
+++ EcommerceApp/src/screens/Cart.js
@@ -3,8 +3,9 @@ import { View, StyleSheet, ScrollView } from 'react-native';
 
 import { Text } from '../components/Text';
 import colors from '../constants/colors';
-import { useCart } from '../util/cart';
+import { useCart, cartTotal } from '../util/cart';
 import { CartRow } from '../components/CartRow';
+import { money } from '../util/format';
 
 const styles = StyleSheet.create({
   emptyContainer: {
@@ -13,12 +14,17 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     paddingTop: 60,
   },
+  summaryContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderTopColor: colors.border,
+    borderTopWidth: StyleSheet.hairlineWidth,
+  },
 });
 
 export const Cart = () => {
   const { cart } = useCart(state => ({ cart: state.cart }));
 
-  console.log('cart', cart);
   const isEmpty = Object.keys(cart).length === 0;
 
   if (isEmpty) {
@@ -36,6 +42,12 @@ export const Cart = () => {
       {Object.keys(cart).map(id => (
         <CartRow key={id} id={id} />
       ))}
+      <View style={styles.summaryContainer}>
+        <Text>
+          <Text style={{ fontWeight: 'bold' }}>Total: </Text>
+          {money(cartTotal(cart))}
+        </Text>
+      </View>
     </ScrollView>
   );
 };
