5ea6782abcc0d3aa168831a49f0ba32494303f4e
Spencer Carli <spencercarli@gmail.com>
2021-07-26 15:05:03 -0500

    Cart Management: Persist Cart between Sessions

--- EcommerceApp/src/util/cart.js
+++ EcommerceApp/src/util/cart.js
@@ -1,42 +1,52 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const useCart = create(set => ({
-  cart: {},
-  addItem: ({ id, name, price }) => {
-    set(state => {
-      // cart.123 = { name: 'hello', price: 1999 }
+export const useCart = create(
+  persist(
+    set => ({
+      cart: {},
+      addItem: ({ id, name, price }) => {
+        set(state => {
+          // cart.123 = { name: 'hello', price: 1999 }
 
-      const cart = { ...state.cart };
-      if (!cart[id]) {
-        cart[id] = {
-          id,
-          name,
-          price,
-          quantity: 0,
-        };
-      }
+          const cart = { ...state.cart };
+          if (!cart[id]) {
+            cart[id] = {
+              id,
+              name,
+              price,
+              quantity: 0,
+            };
+          }
 
-      cart[id].quantity += 1;
+          cart[id].quantity += 1;
 
-      return { cart };
-    });
-  },
-  removeItem: id => {
-    set(state => {
-      const cart = { ...state.cart };
+          return { cart };
+        });
+      },
+      removeItem: id => {
+        set(state => {
+          const cart = { ...state.cart };
 
-      if (!cart[id]) {
-        return { cart };
-      }
+          if (!cart[id]) {
+            return { cart };
+          }
 
-      const newQuantity = cart[id].quantity - 1;
-      if (newQuantity <= 0) {
-        delete cart[id];
-      } else {
-        cart[id].quantity = newQuantity;
-      }
+          const newQuantity = cart[id].quantity - 1;
+          if (newQuantity <= 0) {
+            delete cart[id];
+          } else {
+            cart[id].quantity = newQuantity;
+          }
 
-      return { cart };
-    });
-  },
-}));
+          return { cart };
+        });
+      },
+    }),
+    {
+      name: 'cart',
+      getStorage: () => AsyncStorage,
+    },
+  ),
+);
