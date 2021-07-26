995ae40b7151b2dff1a556e415002243fbcce785
Spencer Carli <spencercarli@gmail.com>
2021-07-26 13:00:32 -0500

    Cart Management: Empty Cart State

--- EcommerceApp/src/screens/Cart.js
+++ EcommerceApp/src/screens/Cart.js
@@ -1,1 +1,24 @@
-export const Cart = () => null;
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { Text } from '../components/Text';
+import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    paddingTop: 60,
+  },
+});
+
+export const Cart = () => {
+  return (
+    <View style={styles.emptyContainer}>
+      <Text type="header" style={{ fontSize: 25 }}>
+        EMPTY CART
+      </Text>
+    </View>
+  );
+};
